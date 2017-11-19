ff253a61b2f3a20f990b2e192d9cd6ffd23f9c75
Pierre Guillaume <pierre@tricy.io>
2017-11-20 00:17:07 +0100

    using nwb to build the project

--- src/lib/index.js
+++
@@ -1,9 +0,0 @@
-import IntlInput from './components/intl-input';
-import ReduxFormIntlInput from './components/redux-form-intl-input';
-import Option from './components/option';
-
-export {
-  IntlInput,
-  ReduxFormIntlInput,
-  Option,
-};
